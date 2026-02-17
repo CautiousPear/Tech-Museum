76e43682cadad6e331081b8e35dd581066d24752
CautiousPear <gtnevsimal@gmail.com>
2026-02-17 16:42:53 -0700

    Add IBM calculator cool

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const museumObjects = [ //List of objects with their links, images, and alt text
     { link: "pages/mechanical_adding_machine.html", img: "images/mechanical_adding_machine.png", alt: "Mechanical Adding Machine" },
     { link: "pages/dead_reckoning_computer.html", img: "images/dead_reckoning_computer.png", alt: "Dead-reckoning Computer" },
     { link: "pages/sony_mavica_fd100.html", img: "images/sony_mavica_fd100.png", alt: "Sony Mavica FD100" },
-    { link: "pages/sony_mavica_fd97.html", img: "images/sony_mavica_fd97.png", alt: "Sony Mavica FD97" }
-
+    { link: "pages/sony_mavica_fd97.html", img: "images/sony_mavica_fd97.png", alt: "Sony Mavica FD97" },
+    { link: "pages/ibm_calculator_tool.html", img: "images/ibm_machine_load_computer_slide_rule.png", alt: "IBM Calculator Tool" }
   ];
 
 const ITEMS_PER_SHELF = window.innerWidth < 600 ? 3 : 6; 
