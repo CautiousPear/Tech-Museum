1fa32b69fa5d999270d9f7b96f11fe91ea8cf870
CautiousPear <gtnevsimal@gmail.com>
2026-02-16 23:01:55 -0700

    Restructured main page, added .glb files, began creating pages.

---
+++ script.js
@@ -0,0 +1,42 @@
+const museumObjects = [ //List of objects with their links, images, and alt text
+
+    { link: "pages/object-1.html", img: "images/VictrolaMiniBoombox.png", alt: "Victrola Mini Boombox" },
+    { link: "pages/underwood_typewriter.html", img: "images/underwood_typewriter.png", alt: "Underwood Typewriter" },
+    { link: "pages/ibm_disk_drives.html", img: "images/ibm_disk_drives.png", alt: "IBM Disk Drives" },
+    { link: "pages/balopticon.html", img: "images/balopticon.png", alt: "Balopticon" },
+    { link: "pages/mechanical_adding_machine.html", img: "images/mechanical_adding_machine.png", alt: "Mechanical Adding Machine" },
+    { link: "pages/dead_reckoning_computer.html", img: "images/dead_reckoning_computer.png", alt: "Dead-reckoning Computer" },
+    { link: "pages/sony_mavica_fd100.html", img: "images/sony_mavica_fd100.png", alt: "Sony Mavica FD100" },
+    { link: "pages/sony_mavica_fd97.html", img: "images/sony_mavica_fd97.png", alt: "Sony Mavica FD97" }
+
+  ];
+
+const ITEMS_PER_SHELF = window.innerWidth < 600 ? 3 : 6; 
+
+  const mainFloor = document.getElementById('museum-floor');
+
+  // Group objects into shelves and create HTML for each shelf
+  for (let i = 0; i < museumObjects.length; i += ITEMS_PER_SHELF) {
+    // Create a new shelf div
+    const shelfDiv = document.createElement('div');
+    shelfDiv.className = 'shelf';
+
+    const shelfItems = museumObjects.slice(i, i + ITEMS_PER_SHELF);
+
+    // Create the HTML for each object
+    shelfItems.forEach(obj => {
+      const anchor = document.createElement('a');
+      anchor.href = obj.link;
+      
+      const img = document.createElement('img');
+      img.src = obj.img;
+      img.alt = obj.alt;
+      img.className = 'object';
+      
+      anchor.appendChild(img);
+      shelfDiv.appendChild(anchor);
+    });
+
+    // Add the completed shelf to the page
+    mainFloor.appendChild(shelfDiv);
+  }
