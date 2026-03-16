f1664d399320d69703853cd1cffbf63cd854083d
CautiousPear <gtnevsimal@gmail.com>
2026-03-15 23:14:31 -0700

    Restructure data and page creation to template.html using JS, delete old pages, improve formatting, and create data.js to store object data.

---
+++ data.js
@@ -0,0 +1,262 @@
+export const museumObjects = [
+
+    /*{ link: "pages/underwood_typewriter.html", img: "images/underwood_typewriter.png", alt: "Underwood Typewriter" },
+    { link: "pages/ibm_disk_drives.html", img: "images/ibm_disk_drives.png", alt: "IBM Disk Drives" },
+    { link: "pages/balopticon.html", img: "images/balopticon.png", alt: "Balopticon" },
+    { link: "pages/mechanical_adding_machine.html", img: "images/mechanical_adding_machine.png", alt: "Mechanical Adding Machine" },
+    { link: "pages/dead_reckoning_computer.html", img: "images/dead_reckoning_computer.png", alt: "Dead-reckoning Computer" },
+    { link: "pages/sony_mavica_fd100.html", img: "images/sony_mavica_fd100.png", alt: "Sony Mavica FD100" },
+    { link: "pages/sony_mavica_fd97.html", img: "images/sony_mavica_fd97.png", alt: "Sony Mavica FD97" },
+    { link: "pages/ibm_calculator_tool.html", img: "images/ibm_machine_load_computer_slide_rule.png", alt: "IBM Calculator Tool" },
+    { link: "pages/ibm_port_a_punch.html", img: "images/ibm_port_a_punch.png", alt: "IBM Port-a-Punch" },
+    { link: "pages/watson_film_loader.html", img: "images/watson_film_loader.png", alt: "Watson Film Loader" },
+    { link: "pages/sony_mavica_fd73.html", img: "images/sony_mavica_fd73.png", alt: "Sony Mavica FD-73" },
+    { link: "pages/sony_mavica_fd7.html", img: "images/sony_mavica_fd7.png", alt: "Sony Mavica FD-7" },
+    { link: "pages/cassette_recorder_player.html", img: "images/cassette_recorder_player.png", alt: "General Electric Tape Recorder & Player" },
+    { link: "pages/starflex_film_camera.html", img: "images/kodak_starflex.png", alt: "Kodak Starflex Film Camera" },
+    { link: "pages/iomega_zip_drives.html", img: "images/zip_drives.png", alt: "Iomega Zip Drive" },
+    { link: "pages/intel_pentium_II_cpu.html", img: "images/intel_pentium_II_cpu.png", alt: "Intel Pentium II CPU" },
+    { link: "pages/intel_core_2_duo_cpu.html", img: "images/intel_core_2_duo_cpu.png", alt: "Intel Core 2 Duo CPU" },
+    { link: "pages/sony_memory_floppy_adapter.html", img: "images/sony_memory_floppy_adapter.png", alt: "Sony Memory Stick Floppy Adapter" },*/
+    { id: "underwood_typewriter", 
+      title: "Underwood Typewriter (1901-1907)", 
+      model: "../models/underwood_typewriter.glb", 
+      description: `The most popular typewriter of the pre-WWII period, the Underwood No.5 laid down a standard of quality that other typewriter companies were quick to copy. 
+      
+      It worked by using metal rods with small letter and number stamps on their ends to strike an ink ribbon, and leave marks on an inserted sheet of paper.`,
+      img: "images/underwood_typewriter.png", 
+      alt: "Underwood Typewriter", 
+      imageCredit: "Pixabay",
+      imageLink: "https://pixabay.com/photos/vintage-typewriter-underwood-2554338/"
+    },
+
+    { id: "ibm_disk_drives", 
+      title: "IBM Computer Disk Drives (1960s)", 
+      model: "../models/ibm_disk_drives.glb", // Model missing
+      description: `In the early days of computing devices were much bigger, at least in physical size, than they are today and were also mostly IBM brand. The IBM Hard Disk Drive on the top right was 15 inches in diameter, but could only store 1 Megabyte of information!
+      
+      Pictured below is the Memorex Mark XIII Disk Pack. At the time, various companies were making their hard drives plug-compatible, this one was interchangeable with the IBM 2316 Disk Module's Disk Packs.`,
+      img: "images/ibm_disk_drives.png", 
+      alt: "IBM Disk Drives", 
+      imageCredit: "Pixabay",
+      imageLink: "https://www.computerhistory.org/revolution/memory-storage/8/259/1060"
+    },
+
+    { id: "balopticon", 
+      title: "Bausch & Lomb Balopticon (1901)", 
+      model: "../models/balopticon.glb", // Model missing
+      description: `The Balopticon was like an early 1900s version of what the PowerPoint application does today. It was named after the Bausch and Lomb Optical Company, though many knew it by another name: the Magic Lantern.
+      
+      They were widely used in schools to project film images across the country, by photographers or families looking to share their film images, and even some traveling salesmen.
+      
+      The "handles" on the sides are actually part of the film tray, which slides out on either side, to make switching pictures easy.`,
+      img: "images/balopticon.png", 
+      alt: "Balopticon", 
+      imageCredit: "Gibson's Auctions",
+      imageLink: "https://www.gibsonsauctions.com.au/auction-lot/a-vintage-bausch-and-lomb-balopticon-slide-proj_F654B298F6.jpg"
+    },
+
+    { id: "mechanical_adding_machine", 
+      title: "Burroughs Mechanical Adding Machine (1901)", 
+      model: "../models/mechanical_adding_machine.glb", 
+      description: `The adding machine was a revolutionary product in terms of business, as beforehand people had to add manually many different totals in large transactions.
+      
+      It may have been limited to addition, but that didn't stop businesses the country over from using the early calculators in most, if not all of their transactions.
+      
+      The machine pictured below is of a Model 3, made from 1910-1911. The ink ribbon still works after all this time, so feel free to give it a whirl while it's wet! (At out physical museum, of course.)`,
+      img: "images/mechanical_adding_machine.png", 
+      alt: "Mechanical Adding Machine", 
+      imageCredit: "John Wolff's Antique Calculators",
+      imageLink: "https://www.johnwolff.id.au/calculators/Burroughs/Burroughs.htm"
+    },
+
+    { id: "dead_reckoning_computer", 
+      title: "Kane Mark 6b Dead-reckoning computer (1957)", 
+      model: "../models/dead_reckoning_computer.glb", 
+      description: `The dead reckoning computer was a device used to manually calculate your position in the air by integrating velocity and direction over the course of your journey.
+      
+      Nowadays computers handle the job of most of the necessary calculations, but in the 60s and before, you had to calculate everything yourself, so naturally most pilots used tools not unlike this one!`,
+      img: "images/dead_reckoning_computer.png", 
+      alt: "Dead-reckoning Computer", 
+      imageCredit: "Paratrooper",
+      imageLink: "https://www.paratrooper.fr/en/usaaf/54953-computer-dead-reckoning-type-an-5835-i-usaaf-1945-3000000505861.html"
+    },
+
+    { id: "sony_fd100_mavica", 
+      title: "Sony FD-100 Mavica (2002)", 
+      model: "../models/sony_fd100_mavica.glb", 
+      description: `A digital camera made by SONY. It was released in 2002. and used 3&1/2 inch floppy disks for storing photos, as well as memory sticks.
+      
+      You could take video, in addition to pictures, of up to 5 seconds. It may seem incredibly short, but for its time, 5 seconds of video without a camcorder was nearly a miracle`,
+      img: "images/sony_fd100_mavica.png", 
+      alt: "Sony FD-100 Mavica", 
+      imageCredit: "Obsolete Sony",
+      imageLink: "https://obsoletesony.substack.com/p/when-cameras-ran-on-floppy-disks"
+    },
+
+    { id: "sony_fd97_mavica", 
+      title: "Sony FD-97 Mavica (2001)", 
+      model: "../models/sony_fd97_mavica.glb", 
+      description: `This camera could take HD photos (1024 x 70) and write them to either a memory stick, or a floppy drive.
+      
+      You could record 5 second long videos with a floppy drive inserted, or longer with a memory stick.
+      
+      It had image stabilization capabilities, allowing users with shaky hands to take photos without worrying about motion blur.`,
+      img: "images/sony_fd97_mavica.png", 
+      alt: "Sony FD-97 Mavica", 
+      imageCredit: "Obsolete Sony",
+      imageLink: "https://obsoletesony.substack.com/p/when-cameras-ran-on-floppy-disks"
+    },
+
+    { id: "ibm_calculator_tool", 
+      title: "IBM Machine Load Computer Slide Rule (1950s)", 
+      model: "../models/ibm_calculator_tool.glb", 
+      description: `"This plastic rectangular instrument calculated the time required for different types of IBM punched card equipment to process given numbers of cards. 
+      
+      The black side is for accounting machines, sorters, and collators. The white side is for card punches, verifiers, and auxiliary machines. 
+      
+      These machines were in use from roughly 1953 through 1959." (National Museum of American History)`,
+      img: "images/ibm_calculator_tool.png", 
+      alt: "IBM Machine Load Computer Slide Rule", 
+      imageCredit: "National Museum of American History",
+      imageLink: "https://americanhistory.si.edu/collections/object/nmah_1381787"
+    },
+
+    { id: "ibm_port_a_punch", 
+      title: "IBM Port-a-Punch & Punch Cards (1958)", 
+      model: "../models/ibm_port_a_punch.glb", 
+      description: `Used for manually punching cards to enter data into a computer.
+      
+      Other items pictured promotional leaflets, punch cards, and a black info card.`,
+      img: "images/ibm_port_a_punch.png", 
+      alt: "IBM Port-a-Punch & Punch Cards", 
+      imageCredit: "Wikipedia",
+      imageLink: "https://en.wikipedia.org/wiki/File:IBM_Port-A-Punch.jpg"
+    },
+
+    { id: "watson_film_loader", 
+      title: "Watson '66' Bulk Film Loader (c. 1960s-1970s)", 
+      model: "../models/watson_film_loader.glb", // Model missing, but have a low-quality version possible
+      description: `A Watson '66' Bulk Film Loader, from the late 1960s, early 1970s. It was used to load film into cameras, without the use of a darkroom.
+      
+      Normally if film is exposed to light, it's ruined. This device was quite useful in preventing that.`,
+      img: "images/watson_film_loader.png", 
+      alt: "Watson '66' Bulk Film Loader", 
+      imageCredit: "New Wave Pool",
+      imageLink: "https://www.newwavepool.shop/en-us/products/watson-model-100-vintage-35mm-100-bulk-film-loader-by-pfefer-products"
+    },
+
+    { id: "sony_fd73_mavica", 
+      title: "Sony Mavica FD-73 (1999)", 
+      model: "../models/sony_fd73_mavica.glb", 
+      description: `A digital camera made by SONY. It was released on August 24, 1999 for $599, and used 3&1/2 inch floppy disks for storing photos.
+      
+      It also had an email option for pictures. This mode compressed photos to make sending an email containing one quicker.`,
+      img: "images/sony_fd73_mavica.png", 
+      alt: "Sony FD-73 Mavica", 
+      imageCredit: "Digital Camera Museum",
+      imageLink: "https://www.digitalkameramuseum.de/en/cameras/item/sony-mvc-fd73"
+    },
+
+    { id: "sony_fd7_mavica", 
+      title: "Sony Mavica FD-7 (1997)", 
+      model: "../models/sony_fd7_mavica.glb", 
+      description: `This camera was one of the first digital cameras to support proper optical zoom (10x). It was also the second digital camera Sony made. It could take pictures at 640 x 480 resolution, the upper limit of standard definition.
+      
+      The FD in its name stands for Floppy Disk, as 3 & 1/2 inch floppies could be used with the camera.`,
+      img: "images/sony_fd7_mavica.png", 
+      alt: "Sony FD-7 Mavica", 
+      imageCredit: "DPReview",
+      imageLink: "https://www.dpreview.com/articles/2701323663/sonys-mavica-fd71-likes-floppy-disks-hates-magnets"
+    },
+
+    { id: "cassette_recorder_player", 
+      title: "General Electric Tape Recorder & Player 3-5027 (1982)", 
+      model: "../models/cassette_recorder_player.glb", 
+      description: `This cassette player came with a power adapter, but could be played off a set of four AA batteries. When the cassette tape was put inside the player, you could record and play back audio, as well as fast forward, rewind, and stop playback and recording.
+      
+      This device has no display, so you had to guess where you thought you were on the tape while rewinding or fast forwarding.
+      
+      They held 30 to 45 minutes of audio per side, and being double sided doubled the amount of music it could hold.`,
+      img: "images/cassette_recorder_player.png", 
+      alt: "General Electric Tape Recorder & Player 3-5027", 
+      imageCredit: "eBay Listing",
+      imageLink: "https://www.ebay.com/itm/227159814785"
+    },
+
+    { id: "starflex_film_camera", 
+      title: "Kodak Brownie Starflex Film Camera (1957)", 
+      model: "../models/starflex_film_camera.glb", 
+      description: `A film camera by Kodak. It was priced new at just $10, and used rolls of film to store pictures, which needed to be taken to a store to be developed.
+      
+      It came with a one-use flash bulb, and one roll of film. It could also switch between taking color, and black & white photos.
+      
+      It was marketed as a simple, easy-to-use camera for the amateur photographer. For someone who doesn't want to carry about the expensive, weighty, but highly advanced cameras for the time.`,
+      img: "images/starflex_film_camera.png", 
+      alt: "Kodak Brownie Starflex Film Camera", 
+      imageCredit: "Steve Harwood",
+      imageLink: "https://www.flickr.com/photos/captkodak/271837543/in/pool-camerawiki"
+    },
+
+    { id: "iomega_zip_drives", 
+      title: "Iomega Zip Drive and Zip disks (1994)", 
+      model: "../models/iomega_zip_drives.glb", 
+      description: `Iomega released these in late 1994, and for its time the disks used were medium to high capacity drives, with space clocking in at 100 megabytes, and being expanded later on to 250 and 750 megabytes. Hard drives at the time were around 500mb in size, making these a relatively cheap way to back a computer up. While the zip drive itself cost $200, the same as the 500mb hard drive, the disks were relatively cheap at around a quarter of the price.
+      
+      Despite being 3 decades old, they are still used today, in both aeronautical flight control computers, and by vintage tech enthusiasts.
+      
+      Also pictured is an external zip drive, which could be connected to a PC to use Zip disks.
+      
+      There was a controversy around a fault in Zip drive hardware dubbed the "Click of Death". The heads that read the Zip disk would be prone to misalignment over time, and eventually would fail completely, making a rhythmic clicking sound as the heads tried to reset their position over and over.
+      
+      The drive also had a feature which worked on paper like so: if power to the drive was cut unexpectedly, the heads would retract quickly to prevent damage if the disk was pulled out. However the quick motion of the retraction caused the drive to be more likely to fail, and damage the disk in the process, spreading the "Click of Death" to different drives.`,
+      img: "images/iomega_zip_drives.png", 
+      alt: "Iomega Zip Drive and Zip disks", 
+      imageCredit: "bhphotovideo",
+      imageLink: "https://www.bhphotovideo.com/c/product/570302-REG/Iomega_31653_Zip_250MB_External_USB.html/reviews"
+    },
+
+    { id: "pentium_ii_cpu", 
+      title: "Intel Pentium II CPU (1997)", 
+      model: "../models/intel_pentium_ii_cpu.glb", // Model missing
+      description: `The Pentium II is a 6th generation x86 microprocessor, released in 1997. It was the successor to the original Pentium, and was succeeded by the Pentium III in 1999. The Pentium II was a significant improvement over the original Pentium, combining the new P6 architecture also found on the Pentium Pro and support for MMX technology.
+      
+      It contains 7.5 million transistors, over double the 3.1 million in the original Pentium, but far from the 10-25 billion in most modern CPUs!`,
+      img: "images/intel_pentium_ii_cpu.png", 
+      alt: "Intel Pentium II CPU", 
+      imageCredit: "Wikimedia Commons",
+      imageLink: "https://commons.wikimedia.org/wiki/File:Intel_Pentium_II_350_MHz.jpg"
+    }, // Would like to add a table later for specs
+
+    { id: "intel_core_2_duo_cpu", 
+      title: "Intel Core 2 Duo (2006)", 
+      model: "../models/intel_core_2_duo_cpu.glb", 
+      description: `The Intel Core 2 Duo, introduced in 2006, was a groundbreaking 64-bit dual-core processor family that significantly boosted performance over previous Pentium models using the efficient Core microarchitecture.
+      
+      These processors, often utilizing Socket 775, are largely obsolete for modern gaming but remain suitable for basic retro computing, lightweight tasks, and older operating systems like Windows XP.`,
+      img: "images/intel_core_2_duo_cpu.png", 
+      alt: "Intel Core 2 Duo CPU", 
+      imageCredit: "memory4less",
+      imageLink: "https://www.memory4less.com/intel-2-66ghz-core2-duo-desktop-processor-e820"
+    }, // Would like to add a table later for specs
+
+    { id: "sony_memory_floppy_adapter", 
+      title: "Sony Memory Stick to Floppy Disk Adapter (2000)", 
+      model: "../models/sony_memory_floppy_adapter.glb", // Model missing
+      description: `This adapter allowed the usage of memory sticks on older computers with only floppy drives and compatible Mavica cameras, when used with compatible software installed.`,
+      img: "images/sony_memory_floppy_adapter.png", 
+      alt: "Sony Memory Stick to Floppy Disk Adapter", 
+      imageCredit: "eBay seller",
+      imageLink: "https://www.ebay.com/itm/325636560397"
+    }, 
+
+    { id: "pny_smart_media_card", 
+      title: "PNY Smart Media card (1995)", 
+      model: "../models/pny_smart_media_card.glb", // Model missing
+      description: `Marketed as the successor to floppy disks, it came as big as 128Mb.`,
+      img: "images/pny_smart_media_card.png", 
+      alt: "PNY Smart Media Card", 
+      imageCredit: "MemoryC",
+      imageLink: "https://www.memoryc.com/3328-128mb-pny-smartmedia-memory-card.html"
+    }
+  ];
--- script.js
+++ script.js
@@ -1,65 +1,47 @@
-const museumObjects = [ //List of objects with their links, images, and alt text
+import { museumObjects } from './data.js';
 
-    { link: "pages/underwood_typewriter.html", img: "images/underwood_typewriter.png", alt: "Underwood Typewriter" },
-    { link: "pages/ibm_disk_drives.html", img: "images/ibm_disk_drives.png", alt: "IBM Disk Drives" },
-    { link: "pages/balopticon.html", img: "images/balopticon.png", alt: "Balopticon" },
-    { link: "pages/mechanical_adding_machine.html", img: "images/mechanical_adding_machine.png", alt: "Mechanical Adding Machine" },
-    { link: "pages/dead_reckoning_computer.html", img: "images/dead_reckoning_computer.png", alt: "Dead-reckoning Computer" },
-    { link: "pages/sony_mavica_fd100.html", img: "images/sony_mavica_fd100.png", alt: "Sony Mavica FD100" },
-    { link: "pages/sony_mavica_fd97.html", img: "images/sony_mavica_fd97.png", alt: "Sony Mavica FD97" },
-    { link: "pages/ibm_calculator_tool.html", img: "images/ibm_machine_load_computer_slide_rule.png", alt: "IBM Calculator Tool" },
-    { link: "pages/ibm_port_a_punch.html", img: "images/ibm_port_a_punch.png", alt: "IBM Port-a-Punch" },
-    { link: "pages/watson_film_loader.html", img: "images/watson_film_loader.png", alt: "Watson Film Loader" },
-    { link: "pages/sony_mavica_fd73.html", img: "images/sony_mavica_fd73.png", alt: "Sony Mavica FD-73" },
-    { link: "pages/sony_mavica_fd7.html", img: "images/sony_mavica_fd7.png", alt: "Sony Mavica FD-7" },
-    { link: "pages/cassette_recorder_player.html", img: "images/cassette_recorder_player.png", alt: "General Electric Tape Recorder & Player" },
-    { link: "pages/starflex_film_camera.html", img: "images/kodak_starflex.png", alt: "Kodak Starflex Film Camera" },
-    { link: "pages/iomega_zip_drives.html", img: "images/zip_drives.png", alt: "Iomega Zip Drive" },
-    { link: "pages/intel_pentium_II_cpu.html", img: "images/intel_pentium_II_cpu.png", alt: "Intel Pentium II CPU" },
-    { link: "pages/intel_core_2_duo_cpu.html", img: "images/intel_core_2_duo_cpu.png", alt: "Intel Core 2 Duo CPU" }
-  ];
+const mainFloor = document.getElementById('museum-floor');
 
-  const mainFloor = document.getElementById('museum-floor');
 
-  function renderMuseum() {
+function renderMuseum() {
 
-    mainFloor.innerHTML = '';
+  mainFloor.innerHTML = '';
 
-    let itemsPerShelf;
-    if (window.innerWidth < 600) {
-      itemsPerShelf = 3; // Fewer items for small phones
-    } else if (window.innerWidth < 1000) {
-      itemsPerShelf = 4; // Mid-size for smaller devices
-    } else {
-      itemsPerShelf = 6; // Standard for desktop
-    }
-
-    // Group objects into shelves and create HTML for each shelf
-    for (let i = 0; i < museumObjects.length; i += itemsPerShelf) {
-      // Create a new shelf div
-      const shelfDiv = document.createElement('div');
-      shelfDiv.className = 'shelf';
-
-      const shelfItems = museumObjects.slice(i, i + itemsPerShelf);
-
-      // Create the HTML for each object
-      shelfItems.forEach(obj => {
-        const anchor = document.createElement('a');
-        anchor.href = obj.link;
-          
-        const img = document.createElement('img');
-        img.src = obj.img;
-        img.alt = obj.alt;
-        img.className = 'object';
-          
-        anchor.appendChild(img);
-        shelfDiv.appendChild(anchor);
-      });
-
-      // Add the completed shelf to the page
-      mainFloor.appendChild(shelfDiv);
-    }
+  let itemsPerShelf;
+  if (window.innerWidth < 600) {
+    itemsPerShelf = 3; // Fewer items for small phones
+  } else if (window.innerWidth < 1000) {
+    itemsPerShelf = 4; // Mid-size for smaller devices
+  } else {
+    itemsPerShelf = 6; // Standard for desktop
   }
+
+  // Group objects into shelves and create HTML for each shelf
+  for (let i = 0; i < museumObjects.length; i += itemsPerShelf) {
+    // Create a new shelf div
+    const shelfDiv = document.createElement('div');
+    shelfDiv.className = 'shelf';
+
+    const shelfItems = museumObjects.slice(i, i + itemsPerShelf);
+
+    // Create the HTML for each object
+    shelfItems.forEach(obj => {
+      const anchor = document.createElement('a');
+      anchor.href = `template.html?id=${obj.id}`;
+
+      const img = document.createElement('img');
+      img.src = obj.img;
+      img.alt = obj.alt;
+      img.className = 'object';
+
+      anchor.appendChild(img);
+      shelfDiv.appendChild(anchor);
+    });
+
+    // Add the completed shelf to the page
+    mainFloor.appendChild(shelfDiv);
+  }
+}
 
 renderMuseum();
 
